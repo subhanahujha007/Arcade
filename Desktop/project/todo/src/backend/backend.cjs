db4381a8c6757c2689f296fb3c31588406f9537a
subhanshu jha <subhanshujha001@gmail.com>
2023-12-23 22:23:43 +0530

    first commit

---
+++ Desktop/project/todo/src/backend/backend.cjs
@@ -0,0 +1,60 @@
+
+const express =require('express')
+  const bodyParser = require('body-parser');
+  const cors=require('cors');
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.json());
+  
+  let todos = [];
+  
+  app.get('/todos', (req, res) => {
+    res.json(todos);
+  });
+  
+  app.get('/todos/:id', (req, res) => {
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) {
+      res.status(404).send();
+    } else {
+      res.json(todo);
+    }
+  });
+  let ctr=0;
+  app.post('/todos', (req, res) => {
+    const newTodo = {
+      id: ctr++, // unique random id
+      title: req.body.title,
+      description: req.body.description
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+  });
+  
+  app.put('/todos/:id', (req, res) => {
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      todos[todoIndex].title = req.body.title;
+      todos[todoIndex].description = req.body.description;
+      res.json(todos[todoIndex]);
+    }
+  });
+ 
+  app.delete('/todos/:id', (req, res) => {
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      todos.splice(todoIndex, 1);
+      res.status(200).send();
+    }
+  });
+  
+  // for all other routes, return 404
+  app.use((req, res, next) => {
+    res.status(404).send();
+  });
+  
+  app.listen(3000);
