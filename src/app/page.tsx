5629d31eca2dcf24ba197ec4be11318e36ce84a9
subhanshu jha <subhanshujha001@gmail.com>
2024-07-06 16:46:06 +0530

    added ci/cd and page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import GameCanvas from '../Componenets/GameCanvas';
 const Home = () => {
   return (
     <div className="container">
-      <h1 style={{marginBottom:"20px",border:"2px solid white"}} >Arcade Game</h1>
+      <h1 style={{marginBottom:"20px",border:"2px solid white"}} >::Arcade Game::</h1>
       
       <GameCanvas/>
     </div>
